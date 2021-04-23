965de448a2d189dd950cb7cfb79b3ad2dc0e2935
safwan <safwan@csc.net.my>
2021-04-23 11:07:57 +0800

    alk dashboard

--- resources/views/components/sidebar/nav-item.blade.php
+++ resources/views/components/sidebar/nav-item.blade.php
@@ -1,11 +1,11 @@
 
 <li class="relative px-2 py-1 ">
-    <a class="inline-flex items-center w-full text-sm font-semibold   cursor-pointer hover:text-indigo-600
+    <a class="inline-flex items-center w-full text-sm font-semibold   cursor-pointer 
         @if(Route::current()->uri == $uri)
         bg-indigo-600 p-1 rounded-lg text-white
         @else
         transform  hover:scale-105 transition duration-300 
-        text-gray-500
+        text-gray-500 hover:text-indigo-600
         @endif" href="{{$route}}">
         {{$slot}}
         <span class="ml-4">{{$title}}</span>
