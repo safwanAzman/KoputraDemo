c836279166dbd62bdd36bc72640c080dde30b4ae
safwan <safwan@csc.net.my>
2021-04-26 23:22:52 +0800

    homestay booking

--- resources/views/livewire/page/member/produk/homestay/cameron-booking.blade.php
+++ resources/views/livewire/page/member/produk/homestay/cameron-booking.blade.php
@@ -2,14 +2,33 @@
     <div class="relative z-20 px-0 mx-auto my-3 text-lg font-medium leading-6 text-cool-gray-900">
         <div class="bg-white rounded-lg shadow">
             <main class="px-4 py-6 my-8">
-                <div class="container mx-auto">
+                <div>
                     <div class="flex flex-col mt-2 lg:flex-row">
                         <div class="order-2 w-full lg:w-1/2">
                             <h1>TEST</h1>
+                            <div class="border-2 mt-4 rounded-md p-4">
+                                <x-general.grid mobile="1" gap="5" sm="1" md="2" lg="2" xl="2" class="col-span-6 mt-6">
+                                    <div class="flex space-x-2">
+                                        <x-form.input type="date" label="Start Date" value="" />
+                                        <x-form.input type="date" label="End Date" value="" />
+                                    </div>
+
+                                </x-general.grid>
+                                <x-general.grid mobile="1" gap="5" sm="1" md="2" lg="2" xl="2" class="col-span-6 mt-6">
+                                    <x-form.input label="Test" value="" />
+                                    <x-form.input label="Test" value="" />
+                                    <x-form.input label="Test" value="" />
+                                    <x-form.input label="Test" value="" />
+                                </x-general.grid>
+                            </div>
                         </div>
-                        <div class="flex-shrink-0 order-1 w-full mt-4 mb-8 lg:w-1/2 lg:mb-0 lg:order-2">
-                            <div id='calendar'></div>
+
+                        <div class=" mx-6 flex-shrink-0 order-1 w-full mt-4 mb-8 lg:w-1/2 lg:mb-0 lg:order-2">
+                            <div class="border-2 mt-6 rounded-md p-4">
+                                <div id='calendar'></div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </main>
@@ -18,7 +37,7 @@
 
     <!-- header -->
     <div class="absolute top-0 left-0 right-0 z-0">
-        <x-general.header bgimg=""  title="Booking"/>
+        <x-general.header bgimg="" title="Booking" />
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
