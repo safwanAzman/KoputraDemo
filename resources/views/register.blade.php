2533fc57af60341ce0df5b292d218fff7805a8cb
safwan <safwan@csc.net.my>
2021-05-21 14:42:46 +0800

    add register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -47,19 +47,26 @@
                         </div>
                     </a>
                     <div class="flex justify-center">
-                        <h1 class="text-xl md:text-2xl font-bold leading-tight mt-4">Log masuk akaun</h1>
+                        <h1 class="text-xl md:text-2xl font-bold leading-tight mt-4">Daftar Akaun</h1>
                     </div>
                     <form class="mt-6">
                         <div>
                             <div class="mt-1 rounded-md shadow-sm">
-                                <input id="email" name="email" type="email" placeholder="Alamat Emel" required autofocus
+                                <input id="email" name="email" type="email" placeholder="Nama" required autofocus
                                     class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-indigo-800 focus:bg-white focus:outline-none " />
                             </div>
                         </div>
 
                         <div class="mt-6">
                             <div class="mt-1 rounded-md shadow-sm">
-                                <input wid="password" type="password" placeholder="Kata Laluan" required
+                                <input type="text" placeholder="No Tel Bimbit" required
+                                    class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-indigo-800 focus:bg-white focus:outline-none " />
+                            </div>
+                        </div>
+
+                        <div class="mt-6">
+                            <div class="mt-1 rounded-md shadow-sm">
+                                <input type="text" placeholder="Alamat Emel" required
                                     class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-indigo-800 focus:bg-white focus:outline-none " />
                             </div>
                         </div>
@@ -69,53 +76,18 @@
                                 <input id="remember" type="checkbox"
                                     class="form-checkbox w-4 h-4 text-indigo-800 transition duration-150 ease-in-out" />
                                 <label for="remember" class="block ml-2 text-sm text-gray-900 leading-5">
-                                    Remember
+                                    Saya bersetuju dengan Terma & Syarat
                                 </label>
-                            </div>
-
-                            <div class="text-sm leading-5">
-                                <a href="#"
-                                    class="text-sm font-semibold text-gray-700 hover:text-indigo-800 focus:text-indigo-800">
-                                    Lupa kata laluan?
-                                </a>
                             </div>
                         </div>
 
                         <div class="mt-6">
                             <span class="block w-full rounded-md shadow-sm">
-                                <div
-                                    class=" relative text-center w-full block bg-indigo-800  focus:bg-indigo-800 text-white font-semibold rounded-lg px-4 py-3 mt-6">
-                                    Log masuk
-
-                                    <a href="{{route('members-produk')}}" class="absolute left-0 text-indigo-800">
-                                        Test
-                                    </a>
-
-                                    <a href="{{route('non-member')}}" class="absolute left-12 text-indigo-800">
-                                        Test
-                                    </a>
-
-                                    <a href="{{route('adminkoputra.members-produk')}}" class="absolute left-24 text-indigo-800">
-                                        Test
-                                    </a>
-
-                                    <a href="{{route('alk.members-produk')}}" class="absolute left-36 text-indigo-800">
-                                        Test
-                                    </a>
-
-                                    <a href="{{route('siskop.members-produk')}}" class="absolute right-0 text-indigo-800">
-                                        Test
-                                    </a>
-                                </div>
-                            </span>
-
-                            <hr class="my-6 border-gray-300 w-full">
-
-                            <p class="mt-8">Memerlukan akaun? 
-                                <a href="{{route('daftar')}}" class="text-indigo-800 hover:text-indigo-900 font-semibold">
-                                    Daftar Akaun
+                                <a href="{{route('wellcome')}}"
+                                    class="relative text-center w-full block bg-indigo-800  focus:bg-indigo-800 text-white font-semibold rounded-lg px-4 py-3 mt-6">
+                                        Hantar Kod Pengesahan
                                 </a>
-                            </p>
+                            </span>
                         </div>
                     </form>
 
